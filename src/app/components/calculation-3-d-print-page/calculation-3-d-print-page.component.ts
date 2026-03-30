bd2d826277142a48228202f25a333604a5923b2a
Freek <sartrys.freek@hotmail.com>
2026-03-30 23:09:55 +0200

    feat(angular): basic fields for calculation

--- src/app/components/calculation-3-d-print-page/calculation-3-d-print-page.component.ts
+++ src/app/components/calculation-3-d-print-page/calculation-3-d-print-page.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormsModule, ɵInternalFormsSharedModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormField, MatLabel } from '@angular/material/select';
 import { LoggerService } from '@jet/services/logger/logger.service';
 import { TranslocoModule } from '@jsverse/transloco';
 import { PageComponent } from '../page/page.component';
@@ -6,14 +9,24 @@ import { PageComponent } from '../page/page.component';
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'jet-calculation-3-d-print',
-  imports: [TranslocoModule, PageComponent],
+  imports: [
+    TranslocoModule,
+    PageComponent,
+    FormsModule,
+    MatFormField,
+    MatInputModule,
+    MatLabel,
+    ɵInternalFormsSharedModule,
+  ],
   templateUrl: './calculation-3-d-print-page.component.html',
   styleUrl: './calculation-3-d-print-page.component.scss',
 })
 export class Calculation3DPrintPageComponent {
   readonly #loggerService = inject(LoggerService);
-
   public constructor() {
     this.#loggerService.logComponentInitialization('Calculation3DPrintPageComponent');
   }
+  printTime: number = 60;
+  costPerKg: number = 20;
+  price: number = this.printTime * this.costPerKg;
 }
